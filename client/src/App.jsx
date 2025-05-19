9c557452e2e206d65a9b4752c84a501e0ca928c5
SujalBazari18 <bazarisujal8815@gmail.com>
2025-05-20 00:10:27 +0530

    Your commit message

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage';
+import LoginPage from './pages/LoginPage';
+
+const App = () => {
+  return (
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/RightSidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const RightSidebar = () => {
+  return (
+    <div>
+      <h1>RightSideBar</h1>
+    </div>
+  )
+}
