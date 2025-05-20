a7230ed38d935a3eeeeac151c1dbe70264a0580b
SujalBazari18 <bazarisujal8815@gmail.com>
2025-05-21 00:53:03 +0530

    Commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React, { use } from 'react'
 import assests, {userDummyData}from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
-const Sidebar = ({selectedUser,setSelectedUser}) => {
+const Sidebar = ({SelectedUser,setSelectedUser}) => {
     const navigate=useNavigate();
   return (
-    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser? "max-md:hidden":''}`}>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${SelectedUser? "max-md:hidden":''}`}>
         <div className='pb-5'>
             <div className='flex justify-between items-center'>
                 <img src={assests.logo} alt="logo" className='max-w-40'/>
@@ -23,9 +23,9 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
             </div>
             <div className='flex flex-col'>
                 {userDummyData.map((user, index) => (
-                    <div onClick={()=>{selectedUser(user)}}
+                    <div onClick={()=>{setSelectedUser(user)}}
                     key={index}
-                    className={`relative flex items-center gap-2 p-2 pl=4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id ===user._id && 'bg-[@282142]/50'}`}>
+                    className={`relative flex items-center gap-2 p-2 pl=4 rounded cursor-pointer max-sm:text-sm ${SelectedUser?._id ===user._id && 'bg-[@282142]/50'}`}>
                         <img src={user?.profilePic || assests.avatar_icon} alt="" className='w-[35px] aspect-[1/1] rounded-full'/>
                         <div className='flex flex-col leading-5'>
                             <p>{user.fullName}</p>
